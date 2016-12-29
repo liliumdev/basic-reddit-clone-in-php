9dd5d157f499e60f2658e831def7841008d63231
Ahmed <ahmed.popovic@gmail.com>
2016-12-29 19:53:41 +0100

    promijenio config za mysql na openshift

--- application/config/config.php
+++ application/config/config.php
@@ -4,9 +4,9 @@ $config['base_url'] = 'http://php-brcip.44fs.preview.openshiftapps.com/'; 			// 
 $config['default_index_controller'] = 'main'; 	// Defaultni controller koji se učitava ako nije specificiran
 $config['default_error_controller'] = 'error'; 	// Defaultni controller koji se učitava pri raznim greškama
 
-$config['mysql_host'] = 'localhost';
+$config['mysql_host'] = 'mysql://mysql:3306/';
 $config['mysql_username'] = 'root';
-$config['mysql_password'] = '';
+$config['mysql_password'] = 'open123shift123pw';
 $config['mysql_db'] = 'basicreddit';
 
 
