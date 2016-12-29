555e4be5e3e439bae15d9f0b045b0bbfd5d3b253
Ahmed <ahmed.popovic@gmail.com>
2016-12-29 20:35:20 +0100

    pokusavam drugi host za openshift

--- application/config/config.php
+++ application/config/config.php
@@ -4,7 +4,7 @@ $config['base_url'] = 'http://php-brcip.44fs.preview.openshiftapps.com/'; 			// 
 $config['default_index_controller'] = 'main'; 	// Defaultni controller koji se učitava ako nije specificiran
 $config['default_error_controller'] = 'error'; 	// Defaultni controller koji se učitava pri raznim greškama
 
-$config['mysql_host'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$config['mysql_host'] = 'phps-brcip2.44fs.preview.openshiftapps.com/';
 $config['mysql_username'] = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $config['mysql_password'] = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $config['mysql_db'] = 'basicreddit';
