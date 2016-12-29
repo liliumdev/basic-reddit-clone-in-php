5c3de16d4a68764528a0bda18ee1164f37f72d62
Ahmed <ahmed.popovic@gmail.com>
2016-12-29 20:10:18 +0100

    jos uvijek pravim config za openshift

--- application/config/config.php
+++ application/config/config.php
@@ -5,7 +5,7 @@ $config['default_index_controller'] = 'main'; 	// Defaultni controller koji se u
 $config['default_error_controller'] = 'error'; 	// Defaultni controller koji se učitava pri raznim greškama
 
 $config['mysql_host'] = 'mysql://mysql:3306/';
-$config['mysql_username'] = 'root';
+$config['mysql_username'] = 'liliumdev';
 $config['mysql_password'] = 'open123shift123pw';
 $config['mysql_db'] = 'basicreddit';
 
